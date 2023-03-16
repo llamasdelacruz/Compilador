dac304ed8fd89c456e5954fcc5bba525f9138e16
llamasdelacruz <tetillamas@gmail.com>
2023-03-15 18:47:04 -0600

    Creacion de las carpetas, la ventana principal y su controlador

---
+++ clases/Lexico.py
@@ -0,0 +1,13 @@
+
+class Lexico():
+
+    def __init__(self):
+        print("Aqui esta la clase para colocar los metodo que vas hacer aldo")
+
+
+
+
+
+
+if __name__ == "__main__":
+    objecto = Lexico()
