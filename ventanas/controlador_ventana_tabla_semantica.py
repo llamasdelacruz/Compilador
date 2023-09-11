2321bfe8132d64954e556966e34e12b35638fb5b
Aldo6890 <aldocastillos433@gmail.com>
2023-09-11 11:09:33 -0600

    Creación del controlador de la tabla semántica

---
+++ ventanas/controlador_ventana_tabla_semantica.py
@@ -0,0 +1,34 @@
+import sys 
+from PyQt5.QtWidgets import QApplication,QDialog,QTableWidget
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+import ctypes
+
+
+class Control_pantalla_tabla_token(QDialog):
+    
+    def __init__(self,ventana_principal,tokens):
+        self.tokens = tokens
+        
+        self.ventana_principal=ventana_principal
+        
+        QDialog.__init__(self)
+        #super().__init__()
+        #Carga la configuracion del archivo .ui en el objeto para su ejecucion
+        uic.loadUi("ventanas/Tabla_semantica_dialog.ui",self)
+        
+        #Comandos para centrar la ventana
+        
+        resolucion=ctypes.windll.user32
+        resolucion_ancho=resolucion.GetSystemMetrics(0)
+        resolucion_alto=resolucion.GetSystemMetrics(1)
+        
+        left=int((resolucion_ancho/2)-(self.frameSize().width()/2))
+        top=int((resolucion_alto/2)-(self.frameSize().height()/2))
+        
+        self.move(left,top)
+        
+    def closeEvent(self, event):
+
+        self.ventana_principal.show()
+          
